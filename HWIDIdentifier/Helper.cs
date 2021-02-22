0b6f27e66d1d94bdc3213ee6ba86301c323efe3e
Sirius-ChrisBohannon <chris.bohannon@siriuscom.com>
2021-02-22 12:46:30 -0600

    First functional commit with reading HWID

---
+++ HWIDIdentifier/Helper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace HWIDIdentifier
+{
+    class Helper
+    {
+        public static class HWID
+        {
+            public static Regedit regeditObject = new Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001");
+            public static readonly string profileKey = "HwProfileGuid";
+
+            public static string GetValue()
+            {
+                return regeditObject.Read(profileKey);
+            }
+        }
+        public class Regedit
+        {
+            private string regeditPath = string.Empty;
+            public Regedit(string regeditPath)
+            {
+                this.regeditPath = regeditPath;
+            }
+
+            public string Read(string keyName)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regeditPath))
+                    {
+                        if (key != null)
+                        {
+                            return key.GetValue(keyName).ToString();
+                        }
+                        else
+                        {
+                            return "Error - Subkey not found.";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return "Error - " + ex.Message;
+                }
+            }
+        }
+    }
+}
