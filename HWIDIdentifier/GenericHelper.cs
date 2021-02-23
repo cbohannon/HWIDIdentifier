190e84cacc4cd02774768054d2641f5e54dc55fd
Chris Bohannon <chrisbohannon@gmail.com>
2021-02-23 08:45:44 -0600

    Adding new files

---
+++ HWIDIdentifier/GenericHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace HWIDIdentifier
+{
+    class GenericHelper
+    {
+        public class Regedit
+        {
+            private string regeditPath = string.Empty;
+            public Regedit(string regeditPath)
+            {
+                this.regeditPath = regeditPath;
+            }
+            public string Read(string keyName)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regeditPath))
+                    {
+                        if (key != null)
+                        {
+                            return key.GetValue(keyName).ToString();
+                        }
+                        else
+                        {
+                            return "Error - Subkey not found.";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return "Error - " + ex.Message;
+                }
+            }
+        }
+    }
+}
---
+++ HWIDIdentifier/ReadHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace HWIDIdentifier
+{
+    class ReadHelper
+    {
+        public static class HWID
+        {
+            public static GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001");
+            public static readonly string profileKey = "HwProfileGuid";
+
+            public static string GetValue()
+            {
+                return regeditObject.Read(profileKey);
+            }
+        }
+        public static class PCGuid
+        {
+            // Note that in the HWIDIdentifier project poperties, I had to uncheck Prefer 32-bit for this method call to work properly
+            public static GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SOFTWARE\Microsoft\Cryptography");
+            public static readonly string machineKey = "MachineGuid";
+
+            public static string GetValue()
+            {
+                return regeditObject.Read(machineKey);
+            }
+        }
+        public static class PCName
+        {
+            public static GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName");
+            public static readonly string nameKey = "ComputerName";
+            public static string GetValue()
+            {
+                return regeditObject.Read(nameKey);
+            }
+        }
+        public static class ProductId
+        {
+            public static GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+            public static readonly string productKey = "ProductID";
+            public static string GetValue()
+            {
+                return regeditObject.Read(productKey);
+            }
+        }
+    }
+}
