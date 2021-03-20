8ec312ffa631060b284bd2b6af3347c966b3f815
Chris Bohannon <chrisbohannon@gmail.com>
2021-03-20 15:02:25 -0500

    Adding more test coverage for GenericHelper

--- HWIDTest/GenericHelperTest.cs
+++ HWIDTest/GenericHelperTest.cs
@@ -23,6 +23,22 @@ namespace HWIDTest
             Assert.IsNotNull(regeditObject.Read(profileKey));
         }
         [TestMethod]
+        public void ReadError()
+        {
+            GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\000001");
+            string profileKey = "HwProfileGuid";
+
+            Assert.AreEqual(regeditObject.Read(profileKey), "Error - Subkey not found.");
+        }
+        [TestMethod]
+        public void ReadErrorException()
+        {
+            GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001");
+            string profileKey = "XHwProfileGuidX";
+
+            Assert.IsTrue(regeditObject.Read(profileKey).Contains("Error - "));
+        }
+        [TestMethod]
         public void WriteNotNull()
         {
             GenericHelper.Regedit regeditObject = new GenericHelper.Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001");
