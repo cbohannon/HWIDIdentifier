afbce4fa507306ee1a85e1eb397cf8a8d1cbae44
Chris Bohannon <chrisbohannon@gmail.com>
2021-04-06 08:02:29 -0500

    Just a little cleanup on comments and using

--- HWIDIdentifier/MainWindow.xaml.cs
+++ HWIDIdentifier/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Management;
 using System.Runtime.CompilerServices;
-using System.Diagnostics.CodeAnalysis;
 
 [assembly: InternalsVisibleTo("FlaUITests")]
 namespace HWIDIdentifier
